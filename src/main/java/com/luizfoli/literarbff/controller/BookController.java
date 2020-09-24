0d0070cdff74600ce9ee318775179ef26663fefb
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-09-24 20:11:42 -0300

    feat: Adicionando configuracao para restricao de acesso

---
+++ src/main/java/com/luizfoli/literarbff/controller/BookController.java
@@ -0,0 +1,16 @@
+package com.luizfoli.literarbff.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
